922c83e65d9ed60ce39dae3416c14670221f0b7f
q-tran14 <tranquangquan.it@gmail.com>
2023-11-22 00:17:54 +0700

    Add RPC in Chainsafe Setting

--- AxI/Assets/WebGLTemplates/Web3GL-2020x/network.js
+++ AxI/Assets/WebGLTemplates/Web3GL-2020x/network.js
@@ -1,9 +1,9 @@
 //You can see a list of compatible EVM chains at https://chainlist.org/
 window.networks = [
   {
-    id: Please enter your chain ID,
-    label: "Please enter your chain i.e Ethereum, Binance, Cronos Please enter your network i.e Mainnet, Testnet",
-    token: "Please enter your chain's native symbol i.e Eth, Cro",
-    rpcUrl: 'Please enter your RPC endpoint',
+    id: 11155111,
+    label: "Sepolia Ethereum",
+    token: "SETH",
+    rpcUrl: 'https://nd-685-826-685.p2pify.com/06f123b122795ac19ef306796ab4056a',
   }
 ]
